ccb2afc90c2fbeb91702e03ee5bb780d9ae9eb17
NAVINTHAKURJI <103242062+NAVINTHAKURJI@users.noreply.github.com>
2022-05-30 13:40:21 +0545

    Create multiply.php

---
+++ multiply.php
@@ -0,0 +1,9 @@
+<?php
+$A = readline('Input A :');
+$B = readline('Input B :');
+$C = readline('Input C :');
+$D = readline('Input D :');
+$E = readline('Input E :');
+$multiply = $A*$B*$C*$D*$E;
+echo 'multiply of A*B*C*D*E is :'. $multiply;
+?>
