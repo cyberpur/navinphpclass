ff95445bd4af107f036185b6a39a191aa982bcf5
NAVINTHAKURJI <103242062+NAVINTHAKURJI@users.noreply.github.com>
2022-05-30 13:09:41 +0545

    Create average.php

---
+++ average.php
@@ -0,0 +1,9 @@
+<?php
+$A = readline('Input A :');
+$B = readline('Input B :');
+$C = readline('Input C :');
+$D = readline('Input D :');
+$E = readline('Input E :');
+$sum = $A+$B+$C+$D+$E;
+echo 'Sum of A+B+C+D+E is :'. $sum;
+?>
